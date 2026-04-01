c4cb0a4a7fd9ccf648177ebab627dd3c819233ef
Snehal Pandey <snehalpandey183nit@gmail.com>
2026-04-01 23:40:12 +0530

    updated app

--- app.js
+++ app.js
@@ -82,27 +82,12 @@ app.use((req,res,next) => {
 
 
 
-
-// app.get("/demouser", async (req,res) =>{
-//     let fakeUser = new User ({
-//         email: "student@gmail.com",
-//         username: "delta-user"
-//     });
-//     let registeredUser = await User.register(fakeUser,"helloworld"); 
-//     res.send(registeredUser);
-// });
-
-
-
-
 app.use("/",userRouter);
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 
 
 
-
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderLust";
 
 
 main().then(() => {
